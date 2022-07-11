6063aadf800b99ec1e7387ae07db00fd63bff5fd
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-07-11 22:15:38 +0200

    Delete comments that are not useful

--- contracts/auctions/SimpleAuction.sol
+++ contracts/auctions/SimpleAuction.sol
@@ -3,28 +3,20 @@ pragma solidity ^0.8.4;
 
 contract SimpleAuction {
 
-    // Parameters of the auction. Times are either
-    // absolute unix timestamps (seconds since 1970-01-01)
-    // or time periods in seconds.
     address payable public beneficiary;
     uint public auctionEndTime;
 
-    // Current state of the auction.
     address public highestBidder;
     uint public highestBid;
 
     // Allowed withdrawals of previous bids
     mapping(address => uint) pendingReturns;
 
-    // Set to true at the end, disallows any change.
-    // Byt default initialized to 'false'.
     bool ended;
 
     // Events that will be emitted on changes.
     event HighestBigIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
-
-    // Errors that describe failures.
 
     // The triple-slash comments are so-called natspec comments.
     // They will be shown when the user is asked to 
@@ -39,18 +31,12 @@ contract SimpleAuction {
     /// The function auctionEnd has already been called.
     error AuctionEndAlreadyCalled();
 
-    /// Create a simple auction with 'biddingTime'
-    /// seconds bidding time on behalf of the 
-    /// beneficiary address 'beneficiaryAddress'.
     constructor(uint biddingTime, address payable beneficiaryAddress) {
         beneficiary = beneficiaryAddress;
         auctionEndTime = block.timestamp + biddingTime;
     }
 
     function bid() external payable {
-        // The keyword payable 
-        // is required for the function to
-        // be able to receive Ether.
 
         // Revert the call if the bidding 
         // period is over.
